f4303c55edcbdbf331c3f5ada6d8eeb2a7bd6eed
Omismit <oswaldomismit@gmail.com>
2024-04-19 15:46:08 -0600

    add more tests

--- test/all.js
+++ test/all.js
@@ -1,39 +1,41 @@
 const test = require('brittle')
-const Hyperswarm = require('hyperswarm')
-const b4a = require('b4a')
-const crypto = require('hypercore-crypto')
-
 const list = []
 
 runTests()
 async function runTests () {
-  const test = (await import('brittle')).default
-  test.pause()
+  //const test = (await import('brittle')).default
+  await import('../index');
+  //test.pause()
 
   test('Testing creation of chatrooms', async function (t) {
     await createChatRoom()
+  })
+
+  test('Sending test message in the new chatroom', async function(t){
+    sendMessage('First tes Message')
+    sendMessage('Second test Message')
+    sendMessage('Third test Message')
+  })
+
+  test('Testing cmd selector', async function (t) {
+    await selectCmd(':listroom');
+    await selectCmd(':createroom');
+    await selectCmd(':joinroom 0');
+    await selectCmd(':listroomsss'); ///wrong command
+  })
+
+  test('Create more chatrooms to test', async function (t) {
     await createChatRoom()
     await createChatRoom()
-    test('Testing connections', async function (t) {
-      list.forEach(async (item, index) => {
-        await joinChatRoom(item)
-      })
+    await createChatRoom()
+  })
+
+  test('Testing the Joins to the rooms', async function (t) {
+    list.forEach(async (item, index) => {
+      await joinChatRoom(item)
+      sendMessage('Message rom:',index,'\n')
     })
   })
 
   test.resume()
-}
-
-async function createChatRoom () {
-  const topicBuffer = crypto.randomBytes(32)
-  await joinSwarm(topicBuffer)
-  const topic = b4a.toString(topicBuffer, 'hex')
-  console.log(`[info] Created new chat room: ${topic}`)
-  list.push(topic)
-}
-
-async function joinChatRoom (topicStr) {
-  const topicBuffer = b4a.from(topicStr, 'hex')
-  await joinSwarm(topicBuffer)
-  console.log('[info] Joined chat room')
 }
